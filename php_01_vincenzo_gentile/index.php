f1a01e311f225d4665a9794b25fcf0d37b2a6eab
Vincenzo Gentile <vincen.gentile@gmail.com>
2026-06-14 13:58:49 +0200

    caricato esercizio 1

---
+++ php_01_vincenzo_gentile/index.php
@@ -0,0 +1,25 @@
+<?php
+
+$intero = 42;
+$decimale = 3.14;
+$stringa = "Ciao";
+$booleano = true;
+
+echo "--- VARIABILI ---\n";
+var_dump($intero);
+var_dump($decimale);
+var_dump($stringa);
+var_dump($booleano);
+
+const INTERO2 = 42;
+const DECIMALE2 = 3.14;
+const STRINGA2 = "Ciao Aulab!";
+const BOOLEANO2 = true;
+
+echo "\n--- COSTANTI ---\n";
+var_dump(INTERO2);
+var_dump(DECIMALE2);
+var_dump(STRINGA2);
+var_dump(BOOLEANO2);
+
+?>
